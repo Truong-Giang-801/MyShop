2c878291c19b3440e02cba669ee0b9cd84ec4fac
vietlinhg4 <vietlinhg7@gmail.com>
2024-04-04 01:01:08 +0700

    add LoginWindow

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Windows.Storage;
+
+
+namespace MyShop
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void SaveLoginInfo(string username, string password)
+        {
+            var localSettings = ApplicationData.Current.LocalSettings;
+            localSettings.Values["username"] = username;
+            localSettings.Values["password"] = password; // Lưu password mã hóa
+        }
+
+    }
+}
