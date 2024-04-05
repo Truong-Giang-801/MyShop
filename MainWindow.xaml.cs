1155f47a280678c506cfb5a9d6bca74f0fd95376
Truong-Giang-801 <giangle8013@gmail.com>
2024-04-05 16:51:08 +0700

    update UI ( update button go to each page)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -62,16 +62,41 @@ namespace MyShop
             Payment.Foreground = Brushes.White;
             Support.Background = Brushes.Transparent;
             Support.Foreground = Brushes.White;
-            Sale.Background = Brushes.Transparent;
-            Sale.Foreground = Brushes.White;
+            Account.Background = Brushes.Transparent;
+            Account.Foreground = Brushes.White;
             Setting.Background = Brushes.Transparent;
             Setting.Foreground = Brushes.White;
             Products.Background = Brushes.Transparent;
             Products.Foreground = Brushes.White;
+            Exit_button.Background = Brushes.Transparent;
+            Exit_button.Foreground = Brushes.White;
+        }
+        private void setVisibleOff()
+        {
+            DashboardScreen.Visibility = Visibility.Hidden;
+            PaymentScreen.Visibility = Visibility.Hidden;
+            SupportScreen.Visibility = Visibility.Hidden;
+            ProfileScreen.Visibility = Visibility.Hidden;
+            SettingScreen.Visibility = Visibility.Hidden;
+            ProductScreen.Visibility = Visibility.Hidden;
         }
         private void Exit_button_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            setButtonDashBoard();
+            Exit_button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F7F6F4"));
+            Exit_button.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB7657"));
+            MessageBoxResult result = MessageBox.Show(
+            "Are you sure you want to exit?",
+            "Confirmation",
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                // Perform the action after user confirms.
+                // For example, delete a record or save changes.
+                Application.Current.Shutdown();
+            }
         }
 
         private void Log_Out_Click(object sender, RoutedEventArgs e)
@@ -79,12 +104,25 @@ namespace MyShop
             setButtonDashBoard();
             Log_Out.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F7F6F4"));
             Log_Out.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB7657"));
+            MessageBoxResult result = MessageBox.Show(
+            "Are you sure you want to logout?",
+            "Confirmation",
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Question);
 
+            if (result == MessageBoxResult.Yes)
+            {
+                // Perform the action after user confirms.
+                // For example, delete a record or save changes.
+                Application.Current.Shutdown();
+            }
         }
 
         private void Products_Click(object sender, RoutedEventArgs e)
         {
             setButtonDashBoard();
+            setVisibleOff();
+            ProductScreen.Visibility = Visibility.Visible;
             Products.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F7F6F4"));
             Products.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB7657"));
         }
@@ -92,6 +130,8 @@ namespace MyShop
         private void Payment_Click(object sender, RoutedEventArgs e)
         {
             setButtonDashBoard();
+            setVisibleOff();
+            PaymentScreen.Visibility = Visibility.Visible;
             Payment.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F7F6F4"));
             Payment.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB7657"));
 
@@ -100,32 +140,28 @@ namespace MyShop
         private void Support_Click(object sender, RoutedEventArgs e)
         {
             setButtonDashBoard();
+            setVisibleOff();
+            SupportScreen.Visibility = Visibility.Visible;
             Support.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F7F6F4"));
             Support.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB7657"));
 
         }
 
-        private void InfoCard_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void InfoCard_Loaded_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Sale_Click(object sender, RoutedEventArgs e)
+        private void Profile_Click(object sender, RoutedEventArgs e)
         {
             setButtonDashBoard();
-            Sale.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F7F6F4"));
-            Sale.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB7657"));
+            setVisibleOff();
+            ProfileScreen.Visibility = Visibility.Visible;
+            Account.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F7F6F4"));
+            Account.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB7657"));
 
         }
 
         private void Setting_Click(object sender, RoutedEventArgs e)
         {
             setButtonDashBoard();
+            setVisibleOff();
+            SettingScreen.Visibility = Visibility.Visible;
             Setting.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F7F6F4"));
             Setting.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB7657"));
 
@@ -133,10 +169,11 @@ namespace MyShop
 
         private void DashBoard_Click(object sender, RoutedEventArgs e)
         {
+            setVisibleOff();
             setButtonDashBoard();
+            DashboardScreen.Visibility = Visibility.Visible;
             DashBoard.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F7F6F4"));
             DashBoard.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB7657"));
-
         }
 
         public Products _product;
@@ -149,5 +186,6 @@ namespace MyShop
             this.DataContext = _product;
 
         }
+
     }
 }
